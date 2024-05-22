7e0c5051135773e477f61335e0c6fbd9f2048b5e
bekhzod <icebreaker0210@mail.ru>
2024-05-22 10:21:23 +0200

    Add first commit

---
+++ components/Editor.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import RactMde from "react-mde";
+import Showdown from "showdown";
+
+function Editor({ currentNote, updateNote }) {
+  const [selectedTab, setSelectedTab] = React.useState("write");
+
+  const converter = new Showdown.Converter({
+    tables: true,
+    simplifiedAutoLink: true,
+    strikethrough: true,
+    tasklists: true,
+  });
+
+  return (
+    <section className="pane editor">
+      <RactMde
+        value={currentNote.body}
+        onChange={updateNote}
+        selectedTab={selectedTab}
+        onTabChange={setSelectedTab}
+        generateMarkdownPreview={(markdown) =>
+          Promise.resolve(converter.makeHtml(markdown))
+        }
+        minEditorHeight={80}
+        heightUnits="vh"
+      />
+    </section>
+  );
+}
+export default Editor;
